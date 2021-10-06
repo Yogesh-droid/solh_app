e895a56e2a80d6afea937045ed82d32a3b914730
= <=>
2021-10-06 18:23:44 +0530

    screens and directories added.

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,14 +1,36 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:solh/main.dart';
+import 'package:solh/bottom-navigation/bottom-navigation.dart';
+import 'package:solh/ui/screens/home/homescreen.dart';
 
 @MaterialAutoRouter(
   replaceInRouteName: 'Page,Route',
   routes: <AutoRoute>[
     AutoRoute(
-      path: "/",
-      name: "MyHomePageRouter",
-      page: MyHomePage,
-    ),
+        path: "MasterScreen",
+        name: "MasterScreenRouter",
+        page: MasterScreen,
+        children: [
+          AutoRoute(
+            path: "HomeScreen",
+            name: "HomeScreenRouter",
+            page: HomeScreen,
+          ),
+          AutoRoute(
+            path: "ShareScreen",
+            name: "ShareScreenRouter",
+            page: HomeScreen,
+          ),
+          AutoRoute(
+            path: "ConnectScreen",
+            name: "ConnectScreenRouter",
+            page: HomeScreen,
+          ),
+          AutoRoute(
+            path: "MyGoalsScreen",
+            name: "MyGoalsScreenRouter",
+            page: HomeScreen,
+          ),
+        ]),
   ],
 )
 class $AppRouter {}
--- lib/routes/routes.gr.dart
+++ lib/routes/routes.gr.dart
@@ -7,7 +7,8 @@
 import 'package:auto_route/auto_route.dart' as _i1;
 import 'package:flutter/material.dart' as _i2;
 
-import '../main.dart' as _i3;
+import '../bottom-navigation/bottom-navigation.dart' as _i3;
+import '../ui/screens/home/homescreen.dart' as _i4;
 
 class AppRouter extends _i1.RootStackRouter {
   AppRouter([_i2.GlobalKey<_i2.NavigatorState>? navigatorKey])
@@ -15,20 +16,48 @@ class AppRouter extends _i1.RootStackRouter {
 
   @override
   final Map<String, _i1.PageFactory> pagesMap = {
-    MyHomePageRouter.name: (routeData) => _i1.MaterialPageX<dynamic>(
+    MasterScreenRouter.name: (routeData) => _i1.MaterialPageX<dynamic>(
+        routeData: routeData,
+        builder: (data) {
+          final args = data.argsAs<MasterScreenRouterArgs>(
+              orElse: () => const MasterScreenRouterArgs());
+          return _i3.MasterScreen(index: args.index);
+        }),
+    HomeScreenRouter.name: (routeData) => _i1.MaterialPageX<dynamic>(
         routeData: routeData,
         builder: (_) {
-          return _i3.MyHomePage();
+          return const _i4.HomeScreen();
         })
   };
 
   @override
-  List<_i1.RouteConfig> get routes =>
-      [_i1.RouteConfig(MyHomePageRouter.name, path: '/')];
+  List<_i1.RouteConfig> get routes => [
+        _i1.RouteConfig(MasterScreenRouter.name,
+            path: 'MasterScreen',
+            children: [
+              _i1.RouteConfig(HomeScreenRouter.name, path: 'homeScreen')
+            ])
+      ];
 }
 
-class MyHomePageRouter extends _i1.PageRouteInfo {
-  const MyHomePageRouter() : super(name, path: '/');
+class MasterScreenRouter extends _i1.PageRouteInfo<MasterScreenRouterArgs> {
+  MasterScreenRouter({int? index, List<_i1.PageRouteInfo>? children})
+      : super(name,
+            path: 'MasterScreen',
+            args: MasterScreenRouterArgs(index: index),
+            initialChildren: children);
 
-  static const String name = 'MyHomePageRouter';
+  static const String name = 'MasterScreenRouter';
+}
+
+class MasterScreenRouterArgs {
+  const MasterScreenRouterArgs({this.index});
+
+  final int? index;
+}
+
+class HomeScreenRouter extends _i1.PageRouteInfo {
+  const HomeScreenRouter() : super(name, path: 'homeScreen');
+
+  static const String name = 'HomeScreenRouter';
 }
