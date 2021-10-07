5da8928859b0e756a0c101a02faacf5db1a27ffc
Yuvraj <yuvraj.chaudhry@omlogic.com>
2021-10-07 16:45:32 +0530

    added toggle button and renamed custom buttons file

---
+++ lib/widgets_constants/buttons/toggle_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:solh/widgets_constants/constants/colors.dart';
+
+// ignore: must_be_immutable
+class SolhToggleButton extends StatefulWidget {
+  SolhToggleButton({
+    this.activeColor,
+    this.thumbColor,
+    this.trackColor,
+    required this.switchValue,
+  });
+
+  final Color? activeColor;
+  final Color? thumbColor;
+  final Color? trackColor;
+  bool switchValue; 
+
+  @override
+  _SolhToggleButtonState createState() => _SolhToggleButtonState();
+}
+
+class _SolhToggleButtonState extends State<SolhToggleButton> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoSwitch(
+      activeColor: widget.activeColor ?? SolhColors.green,
+      trackColor: widget.trackColor ?? SolhColors.grey217,
+      thumbColor: widget.thumbColor ?? SolhColors.grey,
+      value: widget.switchValue,
+      onChanged: (value) {
+        setState(() {
+          widget.switchValue = value;
+        });
+      },
+    );
+  }
+}
