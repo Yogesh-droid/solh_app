00382605295a72682a8abf1486a2e2a210d85826
= <=>
2021-10-08 17:41:05 +0530

    started

--- lib/screens/welcome/landing-after-carousel.dart
+++ lib/screens/welcome/landing-after-carousel.dart
@@ -4,14 +4,14 @@ import 'package:solh/widgets_constants/buttons/toggle_button.dart';
 import 'package:solh/widgets_constants/constants/colors.dart';
 import 'package:solh/widgets_constants/constants/textstyles.dart';
 
-class FirstTimeLandingAfterCarousel extends StatefulWidget {
-  const FirstTimeLandingAfterCarousel({ Key? key }) : super(key: key);
+class FirstTimeConsentForm extends StatefulWidget {
+  const FirstTimeConsentForm({Key? key}) : super(key: key);
 
   @override
-  _FirstTimeLandingAfterCarouselState createState() => _FirstTimeLandingAfterCarouselState();
+  _FirstTimeConsentFormState createState() => _FirstTimeConsentFormState();
 }
 
-class _FirstTimeLandingAfterCarouselState extends State<FirstTimeLandingAfterCarousel> {
+class _FirstTimeConsentFormState extends State<FirstTimeConsentForm> {
   bool _switchValue = false;
 
   @override
@@ -21,10 +21,10 @@ class _FirstTimeLandingAfterCarouselState extends State<FirstTimeLandingAfterCar
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         padding: EdgeInsets.symmetric(
-          horizontal: MediaQuery.of(context).size.width/15,
-          vertical: MediaQuery.of(context).size.height/20,
+          horizontal: MediaQuery.of(context).size.width / 15,
+          vertical: MediaQuery.of(context).size.height / 20,
         ),
-        decoration: BoxDecoration(  
+        decoration: BoxDecoration(
           color: SolhColors.white,
         ),
         child: Column(
@@ -32,7 +32,7 @@ class _FirstTimeLandingAfterCarouselState extends State<FirstTimeLandingAfterCar
           children: [
             Padding(
               padding: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height/10,
+                top: MediaQuery.of(context).size.height / 10,
               ),
               child: Text(
                 "Good to have you here",
@@ -41,8 +41,8 @@ class _FirstTimeLandingAfterCarouselState extends State<FirstTimeLandingAfterCar
             ),
             Padding(
               padding: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height/40,
-                bottom: MediaQuery.of(context).size.height/80,
+                top: MediaQuery.of(context).size.height / 40,
+                bottom: MediaQuery.of(context).size.height / 80,
               ),
               child: Text(
                 "Solh Provides insight into your emotional health and how you can work on it. The app is developed by the therapists, but it is important that you understand that it is not a substitute for diagnosis of psychotherapy.",
@@ -62,37 +62,36 @@ class _FirstTimeLandingAfterCarouselState extends State<FirstTimeLandingAfterCar
                     children: [
                       Padding(
                         padding: EdgeInsets.symmetric(
-                          vertical: MediaQuery.of(context).size.height/15,
-                          horizontal: MediaQuery.of(context).size.width/40,
+                          vertical: MediaQuery.of(context).size.height / 15,
+                          horizontal: MediaQuery.of(context).size.width / 40,
                         ),
                         child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                        Expanded(
-                          child: RichText(
-                            text: TextSpan(
-                            text: "I have read and understood ",
-                            style: SolhTextStyles.ToggleParaText,
-                            children: [
-                              TextSpan(
-                            text: "the terms of use ",
-                            style: SolhTextStyles.ToggleLinkText,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Expanded(
+                              child: RichText(
+                                text: TextSpan(
+                                    text: "I have read and understood ",
+                                    style: SolhTextStyles.ToggleParaText,
+                                    children: [
+                                      TextSpan(
+                                        text: "the terms of use ",
+                                        style: SolhTextStyles.ToggleLinkText,
+                                      ),
+                                      TextSpan(
+                                        text: "and ",
+                                        style: SolhTextStyles.ToggleParaText,
+                                      ),
+                                      TextSpan(
+                                        text: "privacy policy",
+                                        style: SolhTextStyles.ToggleLinkText,
+                                      ),
+                                    ]),
                               ),
-                              TextSpan(
-                            text: "and ",
-                            style: SolhTextStyles.ToggleParaText,
-                              ),
-                              TextSpan(
-                            text: "privacy policy",
-                            style: SolhTextStyles.ToggleLinkText,
-                              ),
-                            ]
                             ),
-                                       ),
+                            SolhToggleButton(switchValue: _switchValue),
+                          ],
                         ),
-                     SolhToggleButton(switchValue: _switchValue),
-                    ],
-                  ),
                       ),
                       SolhGreenButton(
                         child: Text(
