b6eaeeea21d93b5a647595f70b54a845f2b11182
= <=>
2021-12-30 20:21:08 +0530

    updated code

--- lib/ui/screens/my-profile/profile/edit-profile.dart
+++ lib/ui/screens/my-profile/profile/edit-profile.dart
@@ -1,12 +1,17 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 import 'package:solh/bloc/user-bloc.dart';
+import 'package:solh/constants/api.dart';
 import 'package:solh/model/user/user.dart';
+import 'package:solh/services/network/network.dart';
 import 'package:solh/services/user/user-profile.dart';
+import 'package:solh/ui/screens/profile-setup/gender-age.dart';
 import 'package:solh/ui/screens/widgets/dropdowns/gender-selection.dart';
 import 'package:solh/widgets_constants/appbars/app-bar.dart';
 import 'package:solh/widgets_constants/buttons/custom_buttons.dart';
+import 'package:solh/widgets_constants/constants/colors.dart';
 import 'package:solh/widgets_constants/constants/textstyles.dart';
 
 class EditMyProfileScreen extends StatefulWidget {
@@ -33,7 +38,8 @@ class _EditMyProfileScreenState extends State<EditMyProfileScreen> {
     userBlocNetwork.getMyProfileSnapshot();
   }
 
-  String? _dropdownValue = "N/A";
+  String? _gender = "N/A";
+  String? _dob = "";
 
   @override
   Widget build(BuildContext context) {
@@ -51,6 +57,9 @@ class _EditMyProfileScreenState extends State<EditMyProfileScreen> {
                 FirebaseAuth.instance.currentUser!.uid),
             builder: (context, userSnapshot) {
               if (userSnapshot.hasData) {
+                _dob = userSnapshot.requireData!.dob;
+                print(userSnapshot.requireData!.gender);
+                _gender = userSnapshot.requireData!.gender;
                 print(userSnapshot.requireData!.firstName);
                 _firstNameTextEditingController.text =
                     userSnapshot.requireData!.firstName;
@@ -71,6 +80,8 @@ class _EditMyProfileScreenState extends State<EditMyProfileScreen> {
                     Container(
                       child: CircleAvatar(
                         radius: 14.w,
+                        backgroundImage: CachedNetworkImageProvider(
+                            userSnapshot.data!.profilePictureUrl),
                       ),
                     ),
                     Container(
@@ -89,21 +100,75 @@ class _EditMyProfileScreenState extends State<EditMyProfileScreen> {
                       textEditingController: _bioTextEditingController,
                       maxLine: 4,
                     ),
-                    GenderSelectionDropdown(
-                        dropDownDecoration: BoxDecoration(border: Border.all()),
-                        dropdownValue: _dropdownValue,
-                        newValue: (String? newValue) {
-                          print(newValue);
-                          _dropdownValue = newValue!;
-                        }),
-                    TextFieldB(
-                      label: "DOB",
-                      textEditingController: _dobTextEditingController,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Gender",
+                          style: TextStyle(color: Color(0xFFA6A6A6)),
+                        ),
+                        GenderSelectionDropdown(
+                          dropDownDecoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(6),
+                              border: Border.all(color: SolhColors.green)),
+                          newValue: (String? newValue) {
+                            print(newValue);
+                            _gender = newValue!;
+                          },
+                          initialDropdownValue:
+                              userSnapshot.requireData!.gender,
+                        ),
+                      ],
                     ),
+                    SizedBox(
+                      height: 2.25.h,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "DOB",
+                          style: TextStyle(color: Color(0xFFA6A6A6)),
+                        ),
+                        DOBPicker(
+                          initialDateOfBirth: DateTime.f,
+                          onChanged: (date) {
+                            print(date);
+                            _dob = date;
+                          },
+                          boxDecoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(6)),
+                              border: Border.all(
+                                color: SolhColors.green,
+                              )),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 1.h,
+                    ),
+                    // TextFieldB(
+                    //   label: "DOB",
+                    //   textEditingController: _dobTextEditingController,
+                    // ),
                     Padding(
                       padding:
                           EdgeInsets.symmetric(horizontal: 4.w, vertical: 4.h),
                       child: SolhGreenButton(
+                        onPressed: () async {
+                          await Network.makeHttpPutRequestWithToken(
+                              url: "${APIConstants.api}/api/edit-user-details",
+                              body: {
+                                "first_name":
+                                    _firstNameTextEditingController.text,
+                                "last_name":
+                                    _lastNameTextEditingController.text,
+                                "gender": _gender,
+                                "bio": _bioTextEditingController.text,
+                                "dob": _dob
+                              });
+                        },
                         child: Text("Save Changes"),
                         height: 6.5.h,
                       ),
