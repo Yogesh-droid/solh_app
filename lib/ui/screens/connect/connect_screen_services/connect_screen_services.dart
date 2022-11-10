980ea8ccad3d1e8bc9ea01d680a9bfca898edde2
Aditya dangi <aditya.dangi@omlogic.com>
2022-11-10 15:41:58 +0530

    connect screen network refactor

--- lib/ui/screens/connect/connect_screen_services/connect_screen_services.dart
+++ lib/ui/screens/connect/connect_screen_services/connect_screen_services.dart
@@ -1,32 +1,24 @@
 import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
 import 'package:solh/constants/api.dart';
+import 'package:solh/controllers/connections/connection_controller.dart';
 import 'package:solh/services/network/network.dart';
 import 'package:solh/ui/screens/connect/connect_sceen_model/connect_screen_model.dart';
 
-class ConnectScreenServices{
-  
-  Future<dynamic>getProfileDetails(String sId)async{
-    try{
-        var response=
-   await Network.makeGetRequestWithToken('${APIConstants.api}/api/user/v1/user-profile/$sId');
-   
-   return ConnectScreenModel.fromJson(response);
-    }
-    catch(e){
+class ConnectScreenServices {
+  ConnectionController connectionController = Get.find();
+  Future<dynamic> getProfileDetails(String sId) async {
+    try {
+      var response = await Network.makeGetRequestWithToken(
+          '${APIConstants.api}/api/user/v1/user-profile/$sId');
+
+      return ConnectScreenModel.fromJson(response);
+    } catch (e) {
       debugPrint(e.toString());
     }
- 
   }
 
+  checkIfAlreadyInConnection() {}
 
-
-  checkIfAlreadyInConnection(){
-
-  }
-
-  checkIfAlreadyInSendConnection(){
-
-  }  
-
-
+  checkIfAlreadyInSendConnection() {}
 }
