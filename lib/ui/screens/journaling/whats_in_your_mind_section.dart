b5f629a7d7d88e86223415607e34af5b37b95e2a
Aditya dangi <aditya.dangi@omlogic.com>
2023-01-23 11:07:55 +0530

    changes in homepage

--- lib/ui/screens/journaling/whats_in_your_mind_section.dart
+++ lib/ui/screens/journaling/whats_in_your_mind_section.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
@@ -38,28 +39,25 @@ class WhatsOnYourMindSection extends StatelessWidget {
           ),
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              InkWell(
-                  onTap: () async {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => MoodMeter()));
-                    FirebaseAnalytics.instance.logEvent(
-                        name: 'MoodMeterOpenTapped',
-                        parameters: {'Page': 'MoodMeter'});
-                  },
-                  child:
-                      SvgPicture.asset('assets/icons/app-bar/mood-meter.svg')),
-              SizedBox(
-                width: 5,
-              ),
+              // InkWell(
+              //     onTap: () async {
+              //       Navigator.push(context,
+              //           MaterialPageRoute(builder: (context) => MoodMeter()));
+              //       FirebaseAnalytics.instance.logEvent(
+              //           name: 'MoodMeterOpenTapped',
+              //           parameters: {'Page': 'MoodMeter'});
+              //     },
+              //     child:
+              // //         SvgPicture.asset('assets/icons/app-bar/mood-meter.svg')),
+              // SizedBox(
+              //   width: 5,
+              // ),
               SolhGreenBorderMiniButton(
-                  height: MediaQuery.of(context).size.height / 18,
-                  width: 60.w,
+                  height: 50,
+                  width: 90.w,
                   alignment: Alignment.centerLeft,
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 4.w,
-                  ),
                   onPressed: () async {
                     await Navigator.push(
                         context,
@@ -69,95 +67,115 @@ class WhatsOnYourMindSection extends StatelessWidget {
                   },
                   //AutoRouter.of(context).push(CreatePostScreenRouter()),
                   child: Obx(() {
-                    return Text(
-                      _journalPageController.selectedGroupId.value == ''
-                          ? "What's on your mind?"
-                          : "post in group",
-                      style: SolhTextStyles.QS_cap_semi,
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 12),
+                          child: Text(
+                            _journalPageController.selectedGroupId.value == ''
+                                ? "What's on your mind?"
+                                : "post in group",
+                            style: SolhTextStyles.QS_cap_semi,
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: SolhColors.primary_green),
+                          child: Center(
+                            child: Icon(
+                              CupertinoIcons.camera,
+                              color: SolhColors.white,
+                            ),
+                          ),
+                        )
+                      ],
                     );
                   })),
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    IconButton(
-                      iconSize: 24,
-                      splashRadius: 20,
-                      padding: EdgeInsets.zero,
-                      onPressed: () async {
-                        final ImagePicker _picker = ImagePicker();
-                        _xFile = await _picker.pickImage(
-                          source: ImageSource.camera,
-                          maxWidth: 640,
-                          maxHeight: 640,
-                          imageQuality: 50,
-                        );
-                        if (_xFile != null) {
-                          final croppedFile = await ImageCropper().cropImage(
-                              sourcePath: _xFile!.path,
-                              aspectRatioPresets: [
-                                CropAspectRatioPreset.square,
-                                // CropAspectRatioPreset.ratio3x2,
-                                // CropAspectRatioPreset.original,
-                                // CropAspectRatioPreset.ratio4x3,
-                                // CropAspectRatioPreset.ratio16x9
-                              ],
-                              // compressQuality:
-                              //     File(_xFile!.path).lengthSync() > 600000
-                              //         ? 20
-                              //         : 100,
-                              compressQuality:
-                                  compression(File(_xFile!.path).lengthSync()),
-                              uiSettings: [
-                                AndroidUiSettings(
-                                    toolbarTitle: 'Edit',
-                                    toolbarColor: SolhColors.white,
-                                    toolbarWidgetColor: Colors.black,
-                                    activeControlsWidgetColor:
-                                        SolhColors.primary_green,
-                                    initAspectRatio:
-                                        CropAspectRatioPreset.square,
-                                    lockAspectRatio: true),
-                                IOSUiSettings(
-                                  minimumAspectRatio: 1.0,
-                                )
-                              ]);
-                          _croppedFile = File(croppedFile!.path);
-                        }
-                        Map<String, dynamic> map =
-                            await _uploadImage(isVideo: false);
-                        // _xFileAsUnit8List = await _croppedFile!.readAsBytes();
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => CreatePostScreen(
-                                  croppedFile: _croppedFile,
-                                  map: map,
-                                )));
-                      },
-                      icon: SvgPicture.asset(
-                          "assets/icons/journaling/post-photo.svg"),
-                      color: SolhColors.primary_green,
-                    ),
-                    Container(
-                      height: 3.h,
-                      width: 0.2,
-                      color: SolhColors.blackop05,
-                    ),
-                    IconButton(
-                      iconSize: 24,
-                      splashRadius: 20,
-                      padding: EdgeInsets.zero,
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => Connections()));
-                      },
-                      icon: SvgPicture.asset("assets/images/connections.svg"),
-                      color: SolhColors.primary_green,
-                    ),
-                  ],
-                ),
-              ),
+              // Expanded(
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+              //     children: [
+              // IconButton(
+              //   iconSize: 24,
+              //   splashRadius: 20,
+              //   padding: EdgeInsets.zero,
+              //   onPressed: () async {
+              //     final ImagePicker _picker = ImagePicker();
+              //     _xFile = await _picker.pickImage(
+              //       source: ImageSource.camera,
+              //       maxWidth: 640,
+              //       maxHeight: 640,
+              //       imageQuality: 50,
+              //     );
+              //     if (_xFile != null) {
+              //       final croppedFile = await ImageCropper().cropImage(
+              //           sourcePath: _xFile!.path,
+              //           aspectRatioPresets: [
+              //             CropAspectRatioPreset.square,
+              //             // CropAspectRatioPreset.ratio3x2,
+              //             // CropAspectRatioPreset.original,
+              //             // CropAspectRatioPreset.ratio4x3,
+              //             // CropAspectRatioPreset.ratio16x9
+              //           ],
+              //           // compressQuality:
+              //           //     File(_xFile!.path).lengthSync() > 600000
+              //           //         ? 20
+              //           //         : 100,
+              //           compressQuality:
+              //               compression(File(_xFile!.path).lengthSync()),
+              //           uiSettings: [
+              //             AndroidUiSettings(
+              //                 toolbarTitle: 'Edit',
+              //                 toolbarColor: SolhColors.white,
+              //                 toolbarWidgetColor: Colors.black,
+              //                 activeControlsWidgetColor:
+              //                     SolhColors.primary_green,
+              //                 initAspectRatio:
+              //                     CropAspectRatioPreset.square,
+              //                 lockAspectRatio: true),
+              //             IOSUiSettings(
+              //               minimumAspectRatio: 1.0,
+              //             )
+              //           ]);
+              //       _croppedFile = File(croppedFile!.path);
+              //     }
+              //     Map<String, dynamic> map =
+              //         await _uploadImage(isVideo: false);
+              //     // _xFileAsUnit8List = await _croppedFile!.readAsBytes();
+              //     Navigator.of(context).push(MaterialPageRoute(
+              //         builder: (context) => CreatePostScreen(
+              //               croppedFile: _croppedFile,
+              //               map: map,
+              //             )));
+              //   },
+              //   icon: SvgPicture.asset(
+              //       "assets/icons/journaling/post-photo.svg"),
+              //   color: SolhColors.primary_green,
+              // // ),
+              // Container(
+              //   height: 3.h,
+              //   width: 0.2,
+              //   color: SolhColors.blackop05,
+              // ),
+              // IconButton(
+              //   iconSize: 24,
+              //   splashRadius: 20,
+              //   padding: EdgeInsets.zero,
+              //   onPressed: () {
+              //     Navigator.push(
+              //         context,
+              //         MaterialPageRoute(
+              //             builder: (context) => Connections()));
+              //   },
+              //   icon: SvgPicture.asset("assets/images/connections.svg"),
+              //   color: SolhColors.primary_green,
+              // ),
+              //     ],
+              //   ),
+              // ),
             ],
           ),
         ),
