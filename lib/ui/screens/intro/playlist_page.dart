5b25dafba1193b2b2ab8c9312c198bfee2889381
Yogesh Kumar <yogesh.kumar@omlogic.com>
2023-03-20 12:21:44 +0530

    playlist page added

--- lib/ui/screens/intro/playlist_page.dart
+++ lib/ui/screens/intro/playlist_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get/instance_manager.dart';
 import 'package:solh/ui/screens/intro/video_tutorial_page.dart';
-
 import '../../../controllers/video/video_tutorial_controller.dart';
 import '../../../widgets_constants/appbars/app-bar.dart';
 import '../../../widgets_constants/constants/textstyles.dart';
