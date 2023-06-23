5c6f1b96756db75cdab23792619dc59728f6f68c
Yogesh-droid <yogesh251193@gmail.com>
2023-06-23 12:56:47 +0530

    some currency issues resolved

--- lib/ui/screens/get-help/allied_booking_continue_details.dart
+++ lib/ui/screens/get-help/allied_booking_continue_details.dart
@@ -77,44 +77,15 @@ class AlliedBookingContinueDetail extends StatelessWidget {
                 'Confirm',
                 style: SolhTextStyles.CTA.copyWith(color: SolhColors.white),
               )),
-        totalPayble: packages.amount == 0 ? 'Free' : "${packages.amount} ",
+        totalPayble: packages.amount == 0
+            ? 'Free'
+            : "${packages.currency} ${packages.amount} ",
         onContinuePressed: () async {
           try {
             Map<String, dynamic> map =
                 await _alliedController.createPackageOrder(packages);
             print('allied**** $map');
             if (map['success']) {
-              // showDialog(
-              //     context: context,
-              //     builder: (context) {
-              //       return Card(
-              //         child: Container(
-              //           width: double.infinity,
-              //           decoration: BoxDecoration(
-              //               image: DecorationImage(
-              //                   fit: BoxFit.fill,
-              //                   image: AssetImage(
-              //                       'assets/images/ScaffoldBackgroundGreen.png'))),
-              //           child: Column(children: [
-              //             Image.asset('assets/images/thankripple.png'),
-              //             Text(
-              //               'Thank You',
-              //               style: SolhTextStyles.QS_head_4.copyWith(
-              //                   color: SolhColors.white),
-              //             ),
-              //             Padding(
-              //               padding: const EdgeInsets.all(18.0),
-              //               child: Text(
-              //                 "Your appointment has been successfully booked on ",
-              //                 style: SolhTextStyles.QS_cap_semi.copyWith(
-              //                     color: Colors.white),
-              //                 textAlign: TextAlign.center,
-              //               ),
-              //             )
-              //           ]),
-              //         ),
-              //       );
-              //     });
               Navigator.pushNamed(context, AppRoutes.paymentscreen, arguments: {
                 "amount": packages.amount,
                 "feeCurrency": packages.currency,
@@ -129,14 +100,6 @@ class AlliedBookingContinueDetail extends StatelessWidget {
               _alliedController.isAlliedBooking(false);
               Future.delayed(Duration(seconds: 2), () {
                 Get.find<AppointmentController>().getUserAppointments();
-                // Navigator.pop(context);
-                // Navigator.pop(context);
-                // Navigator.pop(context);
-                // Navigator.pop(context);
-                // Navigator.push(
-                //     context,
-                //     MaterialPageRoute(
-                //         builder: (context) => AppointmentScreen()));
               });
             } else {
               Utility.showToast(map['message']);
