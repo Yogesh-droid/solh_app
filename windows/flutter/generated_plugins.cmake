4f6e3a620c0f697d3080ce031ba81abed4f63f50
Yogesh Kumar <yogesh.kumar@omlogic.com>
2023-10-19 17:07:30 +0530

    issue list issue resolved

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,7 +6,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   agora_rtc_engine
   connectivity_plus
   file_selector_windows
-  firebase_auth
   firebase_core
   iris_method_channel
   permission_handler_windows
