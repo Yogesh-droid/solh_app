dadc7e55c61ed90d650c7ba51dd86a79cdbde2c4
Adityadangi14 <dangiaditya@gmail.com>
2023-11-03 14:55:03 +0530

    product homepage done

--- lib/ui/screens/products/features/home/ui/controllers/products_home_carousel_controller.dart
+++ lib/ui/screens/products/features/home/ui/controllers/products_home_carousel_controller.dart
@@ -1,3 +1,40 @@
-import 'package:get/get.dart';
+import 'dart:developer';
 
-class ProductsHomeCarouselController extends GetxController {}
+import 'package:get/get.dart';
+import 'package:solh/constants/api.dart';
+import 'package:solh/ui/screens/products/core/data_state/product_data_state.dart';
+import 'package:solh/ui/screens/products/core/request_params/request_params.dart';
+import 'package:solh/ui/screens/products/features/home/domain/entities/product_home_carousel_entity.dart';
+import 'package:solh/ui/screens/products/features/home/domain/usecases/products_home_carousel_usecase.dart';
+import 'package:solh/ui/screens/psychology-test/test_history_details.dart';
+
+class ProductsHomeCarouselController extends GetxController {
+  final ProductsHomeCarouselUsecase productsHomeCarouselUsecase;
+
+  ProductsHomeCarouselController({required this.productsHomeCarouselUsecase});
+
+  var homeCarouselBanners = <ProductHomeCarouselEntity>[].obs;
+  var isLoading = false.obs;
+  var error = ''.obs;
+
+  Future<void> getBanners() async {
+    try {
+      isLoading.value = true;
+      final ProductDataState<List<ProductHomeCarouselEntity>> bannerData =
+          await productsHomeCarouselUsecase.call(RequestParams(
+              url: "${APIConstants.api}/api/product/product-banner"));
+
+      if (bannerData.data != null) {
+        isLoading.value = false;
+        homeCarouselBanners.value = bannerData.data!;
+      } else {
+        isLoading.value = false;
+        error.value = bannerData.exception.toString();
+      }
+    } on Exception catch (e) {
+      log(e.toString());
+      isLoading.value = false;
+      error.value = e.toString();
+    }
+  }
+}
