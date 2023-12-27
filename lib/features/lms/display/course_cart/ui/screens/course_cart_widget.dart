f99eb3462fd6573cf4ffab525616309b3ac85da9
Yogesh-droid <yogesh251193@gmail.com>
2023-12-27 16:34:48 +0530

    course checkout page done

--- lib/features/lms/display/course_cart/ui/screens/course_cart_widget.dart
+++ lib/features/lms/display/course_cart/ui/screens/course_cart_widget.dart
@@ -3,6 +3,9 @@ import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get/instance_manager.dart';
 import 'package:solh/features/lms/display/course_cart/ui/controllers/get_course_cart_controller.dart';
 import 'package:solh/features/lms/display/course_cart/ui/screens/course_items_list.dart';
+import 'package:solh/ui/screens/get-help/get-help.dart';
+import 'package:solh/ui/screens/products/features/product_payment/ui/widgets/payment_details.dart';
+import 'package:solh/ui/screens/products/features/product_payment/ui/widgets/payment_options_tile.dart';
 import 'package:solh/widgets_constants/loader/my-loader.dart';
 
 class CourseCartWidget extends StatelessWidget {
@@ -16,7 +19,16 @@ class CourseCartWidget extends StatelessWidget {
         : SingleChildScrollView(
             child: Column(children: [
               // Cart Items
-              CourseItemsList()
+              const CourseItemsList(),
+              const GetHelpDivider(),
+              PaymentDetails(
+                  total: getCourseCartController.totalPrice.value.toDouble(),
+                  discount: getCourseCartController.grandTotal.value.toDouble(),
+                  currencySymbol:
+                      getCourseCartController.cartList[0].currency ?? '',
+                  horizontalPadding: 8),
+              const GetHelpDivider(),
+              const PaymentOptionsTile(horizontalPadding: 8)
             ]),
           ));
   }
